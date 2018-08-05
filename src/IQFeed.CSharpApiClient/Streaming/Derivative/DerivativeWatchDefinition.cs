72444fc6aac4fe284b415c43b7c9d44ed823b00e
Mathieu Paquette <m.paquette@inovitex.com>
2018-08-05 17:25:48 -0400

    feat(streaming): added Derivative data support

---
+++ src/IQFeed.CSharpApiClient/Streaming/Derivative/DerivativeWatchDefinition.cs
@@ -0,0 +1,16 @@
+﻿namespace IQFeed.CSharpApiClient.Streaming.Derivative
+{
+    public class DerivativeWatchDefinition
+    {
+        public DerivativeWatchDefinition(string symbol, string interval, string requestId)
+        {
+            Symbol = symbol;
+            Interval = interval;
+            RequestId = requestId;
+        }
+
+        public string Symbol { get; }
+        public string Interval { get; }
+        public string RequestId { get; }
+    }
+}
---
+++ src/IQFeed.CSharpApiClient/Streaming/Derivative/SystemMessageExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using IQFeed.CSharpApiClient.Extensions;
+using IQFeed.CSharpApiClient.Streaming.Common.Messages;
+using IQFeed.CSharpApiClient.Streaming.Level1.Messages;
+
+namespace IQFeed.CSharpApiClient.Streaming.Derivative
+{
+    public static class SystemMessageExtensions
+    {
+        private const string DerivativeWatchesPattern = "^S,WATCHES,(.*)$";
+        private static readonly Regex DerivativeWatchesRegex = new Regex(DerivativeWatchesPattern);
+
+        public static IEnumerable<DerivativeWatchDefinition> ToDerivativeWatchDefinitions(this SystemMessage systemMessage)
+        {
+            var match = DerivativeWatchesRegex.Match(systemMessage.Message);
+            if (match.Success)
+            {
+                var watches = match.Groups[1].Value.SplitFeedMessage();
+                var offset = 0;
+
+                while (offset < watches.Length)
+                {
+                    yield return new DerivativeWatchDefinition(watches[offset], watches[offset + 1], watches[offset + 2]);
+                    offset += 3;
+                }
+            }
+        }
+    }
+}
